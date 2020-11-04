ae76e549b90385ad0d7ce5b491e91ef85045bc80
natalizacho <nata2716@stud.kea.dk>
2020-11-04 14:41:36 +0100

    Initial commit

---
+++ src/main/java/dk/kea/jenkinsdockertest/JenkinsDockerTestApplication.java
@@ -0,0 +1,13 @@
+package dk.kea.jenkinsdockertest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsDockerTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JenkinsDockerTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dk/kea/jenkinsdockertest/JenkinsDockerTestApplicationTests.java
@@ -0,0 +1,13 @@
+package dk.kea.jenkinsdockertest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsDockerTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
